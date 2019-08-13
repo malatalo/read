ce0969134156e0e289a683eafc0d635f3705e761
Miikka Alatalo <miikka.alatalo@houston-inc.com>
2019-08-13 15:40:47 +0300

    update

--- script.js
+++ script.js
@@ -1,69 +1,100 @@
 let readInterval = null;
+let currentWordSpeed = 500;
 let wordSpeed = 500;
 let text = "";
 let lastWord = 0;
 let currentWord = 0;
 let loops = false;
 
+const speedChangeStep = 20;
+
+const fonts = {
+  TNR: "Times New Roman,sans-serif",
+  OpenS: "Open Sans,sans-serif",
+  Quicksand: "Quicksand,sans-serif",
+  Roboto: "Roboto,sans-serif",
+  RobotoC: "Roboto Condensed,sans-serif",
+  RobotoM: "Roboto Mono,monospace",
+  SourceS: "Source Sans Pro,sans-serif"
+};
+
 window.onload = () => {
   document.getElementById("wpmVal").innerHTML = document.getElementById("wpm").value;
   document.getElementById("wpm").oninput = () => {
-    wordSpeed = (60 / document.getElementById("wpm").value) * 1000;
-    document.getElementById("wpmVal").innerHTML = document.getElementById("wpm").value;
+    changeWPM();
   };
+  changeWPM();
+};
+
+addSubWPM = type => {
+  let current = parseInt(document.getElementById("wpm").value);
+  if (type === "+") {
+    document.getElementById("wpm").value = current + 50 > 600 ? current : current + 50;
+  } else if (type === "-") {
+    document.getElementById("wpm").value = current - 50 < 50 ? current : current - 50;
+  }
+  changeWPM();
+};
+
+changeWPM = () => {
+  wordSpeed = (60 / document.getElementById("wpm").value) * 1000;
+  document.getElementById("wpmVal").innerHTML = document.getElementById("wpm").value;
 };
 
 start = () => {
-  // const s = document.getElementById("textInput").selectionStart;
-  // const e = document.getElementById("textInput").selectionEnd;
-  // console.log(s === e);
+  console.log(fonts[document.getElementById("font-select").value]);
+  document.getElementById("readText").style.fontFamily =
+    fonts[document.getElementById("font-select").value];
 
+  const s = document.getElementById("textInput").selectionStart;
+  const e = document.getElementById("textInput").selectionEnd;
+
+  window.scrollTo(0, 0);
   loops = true;
-  read();
 
-  let counter = 4;
-  document.getElementById("startBtn").innerHTML = counter + 1;
-  let interval = setInterval(function() {
-    document.getElementById("startBtn").innerHTML = counter;
-    counter--;
-    if (counter == -1) {
-      clearInterval(interval);
-      document.getElementById("startBtn").innerHTML = "READ";
-      // loops = true;
-      // read();
-    }
-  }, 1000);
+  if (s === e) {
+    setTimeout(read(0), 500);
+  } else {
+    setTimeout(read(s), 500);
+  }
 };
 
-read = () => {
+read = subs => {
   text = document
     .getElementById("textInput")
-    .value.split("\n\n")
+    .value.substr(subs)
+    .split("\n\n")
     .join(" #:-DDDd# ")
     .split(" ");
   lastWord = text.length - 1;
   currentWord = 0;
+  currentWordSpeed = 500;
   loops = true;
   loop();
 };
 
 loop = () => {
-  if(!loops){
+  if (!loops) {
     return;
   }
-  if(text[currentWord] == "#:-DDDd#"){
+  if (text[currentWord] == "#:-DDDd#") {
     document.getElementById("readText").innerHTML = " ";
-  }else {
+  } else {
     document.getElementById("readText").innerHTML = text[currentWord];
   }
   if (currentWord === lastWord) {
     stop();
   }
   currentWord++;
-  setTimeout(loop, wordSpeed);
+  if (currentWordSpeed < wordSpeed) {
+    currentWordSpeed += speedChangeStep;
+  } else if (currentWordSpeed > wordSpeed) {
+    currentWordSpeed -= speedChangeStep;
+  }
+  setTimeout(loop, currentWordSpeed);
 };
 
 stop = () => {
   loops = false;
-  setTimeout(document.getElementById("readText").innerHTML = " ", 1000);
+  setTimeout((document.getElementById("readText").innerHTML = " "), 1000);
 };
